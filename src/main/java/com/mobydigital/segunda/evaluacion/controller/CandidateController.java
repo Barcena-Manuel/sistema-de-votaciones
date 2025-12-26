618ff8ad0b693e4f3d2f8f68c4a887d4d2bd5bda
Manuel <manuel.barcena.sto@gmail.com>
2025-12-26 15:44:47 -0300

    Crear los swagger para los endpoints
    Se crearon los swagger para la documentacion de los endpoints

--- src/main/java/com/mobydigital/segunda/evaluacion/controller/CandidateController.java
+++ src/main/java/com/mobydigital/segunda/evaluacion/controller/CandidateController.java
@@ -3,8 +3,10 @@ package com.mobydigital.segunda.evaluacion.controller;
 import com.mobydigital.segunda.evaluacion.dto.CandidateDto;
 import com.mobydigital.segunda.evaluacion.exception.CandidateNotExistException;
 import com.mobydigital.segunda.evaluacion.exception.InvalidDataException;
-import com.mobydigital.segunda.evaluacion.exception.PoliticalPartyNotFoundException;
 import com.mobydigital.segunda.evaluacion.service.CandidateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Candidate", description = "Operations for candidates")
 @RestController
 @RequestMapping("/api/candidate")
 public class CandidateController {
@@ -23,21 +26,52 @@ public class CandidateController {
         this.service = service;
     }
 
+
+    @Operation(summary = "Create candidate",
+            description = "Creates and returns a candidate",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Candidate was created"),
+                    @ApiResponse(responseCode = "400", description = "Invalid data is not accepted")
+            }
+    )
     @PostMapping
-    public ResponseEntity<CandidateDto> createCandidate(@RequestBody CandidateDto candidate) throws InvalidDataException, PoliticalPartyNotFoundException {
+    public ResponseEntity<CandidateDto> createCandidate(@RequestBody CandidateDto candidate) throws InvalidDataException {
         return new ResponseEntity<>(service.create(candidate), HttpStatus.CREATED);
     }
 
+
+    @Operation(summary = "Getting candidates",
+            description = "Obtains a list of all candidates",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "All candidates were obtained"),
+            }
+    )
     @GetMapping
     public ResponseEntity<List<CandidateDto>> getCandidates(){
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Getting candidates by ID",
+            description = "Obtains a candidate by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Candidate was found"),
+                    @ApiResponse(responseCode = "404", description = "The candidate was not found due to an invalid ID")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<CandidateDto> getCandidateById(@PathVariable Long id) throws CandidateNotExistException {
         return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Delete candidate by id",
+            description = "Delete the candidate by ID",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Deleted candidate"),
+                    @ApiResponse(responseCode = "404", description = "The candidate was not removed due to an invalid ID")
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCandidate(@PathVariable Long id) throws CandidateNotExistException{
         service.deleteById(id);
--- src/main/java/com/mobydigital/segunda/evaluacion/controller/PoliticalPartyController.java
+++ src/main/java/com/mobydigital/segunda/evaluacion/controller/PoliticalPartyController.java
@@ -4,6 +4,9 @@ import com.mobydigital.segunda.evaluacion.dto.PoliticalPartyDto;
 import com.mobydigital.segunda.evaluacion.exception.InvalidDataException;
 import com.mobydigital.segunda.evaluacion.exception.PoliticalPartyNotFoundException;
 import com.mobydigital.segunda.evaluacion.service.PoliticalPartyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Political Party", description = "Operations for political party")
 @RestController
 @RequestMapping("/api/politicalParty")
 public class PoliticalPartyController {
@@ -22,21 +26,52 @@ public class PoliticalPartyController {
         this.service = service;
     }
 
+
+    @Operation(summary = "Create political party",
+            description = "Creates and returns a political party",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Political Party was created"),
+                    @ApiResponse(responseCode = "400", description = "Invalid data is not accepted")
+            }
+    )
     @PostMapping
     public ResponseEntity<PoliticalPartyDto> createPoliticalParty(@RequestBody PoliticalPartyDto politicalParty) throws InvalidDataException {
         return new ResponseEntity<>(service.create(politicalParty), HttpStatus.CREATED);
     }
 
+
+    @Operation(summary = "Getting political party",
+            description = "Obtains a list of all politicals parties",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "All political parties were obtained"),
+            }
+    )
     @GetMapping
     public ResponseEntity<List<PoliticalPartyDto>> getAllPoliticalsParties(){
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Getting political party by ID",
+            description = "Obtains a political party by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Political Party was found"),
+                    @ApiResponse(responseCode = "404", description = "The political party was not found due to an invalid ID")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PoliticalPartyDto> getPoliticalPartyById(@PathVariable Long id) throws PoliticalPartyNotFoundException {
         return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Delete political party by id",
+            description = "Delete the political party by ID",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Deleted political party"),
+                    @ApiResponse(responseCode = "404", description = "The political party was not removed due to an invalid ID")
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePoliticalPartyById(@PathVariable Long id) throws PoliticalPartyNotFoundException {
         service.deleteById(id);
